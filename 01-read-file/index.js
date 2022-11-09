ae1bd839dcbbc4b414c870b77124914b3890f650
Roman0891 <106029434+Roman0891@users.noreply.github.com>
2022-11-09 19:15:48 +0300

    Fix: corrected 01-read-file! corrected 02-write -file ! for crosschecks

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,18 +1,18 @@
 
-// const fs = require('fs'); //импортируем/подключаем модуль для работы с файлами копьютера
-// const path = require('path'); // импортируем/подключаем модуль для работы с путями файлам и каталогами
-// const file = path.join(__dirname, 'text.txt'); //file - путь к нему. ( ____dirname -путь к текущей папки)
-// const stream = new fs.createReadStream(file, {encoding: 'utf8'}); //создаем поток ReadStream. кодирование utf8 (чтоб видеть строку)
+const fs = require('fs'); //импортируем/подключаем модуль для работы с файлами копьютера
+const path = require('path'); // импортируем/подключаем модуль для работы с путями файлам и каталогами
+const file = path.join(__dirname, 'text.txt'); //file - путь к нему. ( ____dirname -путь к текущей папки)
+const stream = new fs.createReadStream(file, {encoding: 'utf8'}); //создаем поток ReadStream. кодирование utf8 (чтоб видеть строку)
 
-// stream.on('readable', () => {
-//     let data = stream.read();
-//     if (data !== null) console.log (data);
+stream.on('readable', () => {
+    let data = stream.read();
+    if (data !== null) console.log (data);
+})
 
-
-const fs = require('fs');
-const path = require('path');
-const fileWay = path.join(__dirname, 'text.txt');
-fs.readFile(fileWay, 'utf8',(err, data)=>{
-    console.log(data);
-});
+// const fs = require('fs');
+// const path = require('path');
+// const fileWay = path.join(__dirname, 'text.txt');
+// fs.readFile(fileWay, 'utf8',(err, data)=>{
+//     console.log(data);
+// });
 
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,41 @@
 const fs = require('fs');
-const path = require('path');
-// const fileWay = path.join(__dirname, 'text02.txt')
+const path = require('path'); 
+const readline = require('readline'); //подключаем модульб, который позволяет построчно читать входной поток. Ввод и чтение вывода, предоставленного пользователем.
 
-fs.writeFile('02-write-file/text02.txt', ' не пойму нахера пригодиться', (err)=>{ // создаю фай. параметры. путь/название, что написано в файле/ функция
-    if(err) console.log(err);
-});
+const rl = readline.createInterface({ //создаем консольный интерфейс  ( в него передаем 2 параметра - поток ввод и вывода)
+    
+    input: process.stdin,//поток ввода
+    output: process.stdout,//поток вывода (process) -предопределенная переменная. так созхадли интерфейс.
+
+})
+
+const fileWay = path.join(__dirname, 'text.txt'); // путь к файду
+const fileText = fs.createWriteStream(fileWay);//создаем поток WriteStream.
+
+readline.emitKeypressEvents(process.stdin);
+
+process.stdin.on('keypress', (ch, key) => {  //выход и остановка записи
+    if(key && key.ctrl && key.name === 'c'){
+        console.log('Всего доброго. Запись остановлена!')
+    }
+})
+
+function write(){
+
+    rl.question('Здравствуйте. Напишите текст:', text => {
+        console.log(text);
+        if(text.toLocaleLowerCase()=== "exit"){
+            console.log('Всего доброго. Запись остановлена!');
+            rl.close();
+            return;
+        }
+        fileText.write(text, err =>{
+            if(err) {
+                console.log(err.message);
+            }
+            else{write()}
+        } )
+    })
+}
+write();
+
